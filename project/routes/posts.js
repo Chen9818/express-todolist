00a80ffd780e938bb398722232e25cd8813755ff
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-05-08 00:27:48 +0900

    fix timesort

--- project/routes/posts.js
+++ project/routes/posts.js
@@ -13,6 +13,7 @@ router.get('/',async function(req, res, next) {
         path:"user",  //找到user collection
         select:"name photo"  //顯示name和photo
     })
+	.sort(timeSort)
     handleSuccess(res,post)
 });
 
