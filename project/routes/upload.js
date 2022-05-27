99fa2e902e4538bc37b4460894fb1547dc9fd092
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-05-27 23:19:32 +0900

    add postman

--- project/routes/upload.js
+++ project/routes/upload.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const appError = require("../service/appError");
 const handleErrorAsync = require("../service/handleErrorAsync");
-const sizeOf = require('image-size');
+const sizeOf = require('image-size'); 
 const upload = require('../service/image');
 const { ImgurClient } = require('imgur');
 const {isAuth,generateSendJWT} = require('../service/auth');
 router.post('/', isAuth,upload,handleErrorAsync(async (req, res, next)=> {
+    console.log(req.files)
     if(!req.files.length) {
         return next(appError(400,"尚未上傳檔案",next));
     }
